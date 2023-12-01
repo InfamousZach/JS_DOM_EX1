ddb7d98a5e68f385148924c7172c2cc13af138a9
Zachary A <zachamstutz1@gmail.com>
2023-11-30 19:25:10 -0500

    JS DOM Exercise number one

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const collection = document.getElementsByTagName("header");
+
+const collection1 = document.getElementsByTagName("section");
+
+let collection2 = document.getElementsByClassName('current');
+
+const collection3 = collection2[0].nextElementSibling 
+
+const collection4 = collection2[0].previousElementSibling
+
+const collection5 = document.getElementsByClassName('highlight');
+
+const collection6 = document.querySelectorAll('section h2');
